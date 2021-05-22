f493b3413c2b6e441e177e19ef766636c77eef50
Patrick Braun <prbraun@protonmail.com>
2021-05-22 13:38:06 -0500

    Initializing Repo

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+console.log("Entering script.js");
+//Define some global variables
+// searchHistory as an array 
+//api url
+//api key
+
+//Reference DOM elements 
+//var searchForm = $(""); etc
+
+//Function to push the searchHistory from user input
+    //allows access to the city name when its clicked
+
+//create a function to retrieve search history from local storage
+
+//function to display the current weather data and append information the the page
+
+//create second api call to retrieve the 5 day forcast
+//create variables to grab api response to be able to insert api data into second api
+
+//function to get UV index and render to page
+
+//create a handler to check serach field and return data if no data is in search field return;
